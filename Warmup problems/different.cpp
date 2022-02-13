e552d3df7c03b49185f55e9b025fe8f05146e339
aman043 <dheerajkumarr235@gmail.com>
2022-02-13 21:58:33 +0530

    warmup problems solved

---
+++ Warmup problems/different.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+ 
+int main()
+{
+	ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);	
+	long long a, b;
+	
+	while(cin>>a>>b){
+		cout<<abs(a-b)<<"\n";
+	}
+	return 0;
+}
