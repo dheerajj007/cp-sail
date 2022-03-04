9e9594c8881b6880d9afc65e715b79adccf00808
aman043 <dheerajkumarr235@gmail.com>
2022-03-04 10:41:08 +0530

    update

---
+++ Codeforces Problemset/318a.cpp
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define all(x) (x).begin(), (x).end()
+
+const auto MOD = 1000000007;
+const auto INF = (int)1e9;
+const auto EPS = 1e-9;
+const auto PI = 3.1415926535897932384626433832795;
+ 
+using PII = pair<int, int>;
+using VI = vector<int>;
+using VLL = vector<long long>;
+using VS = vector<string>;
+using VII = vector<PII>;
+using VVI = vector<VI>;
+using MPII = map<int,int>;
+using SETI = set<int>;  	
+using MSETI = multiset<int>; 
+using int64 = long long int;	
+using unint64 = unsigned long long int;
+
+ 
+void solve() {
+	int64 n, k;
+	cin >> n >> k;
+	
+	int64 h = n/2;
+	
+	if(n%2==1) h++;
+	
+	if(k<=h){
+		cout << (k*2)-1;
+	}
+	else cout << (k/2)+1;
+	
+ 
+}
+ 
+int main()
+{
+	ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);	
+	int tc=1;
+	//cin>>tc;
+ 
+	while(tc--){
+		solve();
+	}
+	return 0;
+}
