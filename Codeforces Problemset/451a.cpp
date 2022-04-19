453b34811329d25dba60539e2fd1e4fa40752a8b
aman043 <dheerajkumarr235@gmail.com>
2022-04-19 15:23:03 +0530

    udpate

---
+++ Codeforces Problemset/451a.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+	int n, m;
+	cin >> n >> m;
+	int total = n*m;
+	if(total%2) cout << "Akshat";
+	else cout << "Malvika";
+	return 0;
+}
