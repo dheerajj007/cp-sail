d85f9ff4542d6b183ef329204e293bd37b0998f5
Dheeraj Kumar <dheerajkumarr235@gmail.com>
2022-08-25 09:29:05 +0530

    update

---
+++ Codeforces Problemset/1550a.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define all(x) (x).begin(), (x).end()
+#define ll long long
+
+const ll MOD = 1e9 + 7;
+
+void solve(){
+	int s;
+	cin >> s;
+	int ans = 0;
+
+	for(int i=0; i*i<s; i++){
+		ans++;
+	}
+	cout << ans << endl;
+
+
+	
+}
+ 
+int main(){
+	ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);	
+	
+	int tc=1;
+	cin>>tc;
+	
+	for (int t = 1; t <= tc; t++) {
+		// cout << "Case #" << t << ": ";
+		solve();
+    	}
+ 
+	return 0;
+}
+
+
